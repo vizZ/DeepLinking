46275a66d2cb83c2601553e29091028450e848ed
Artur Glier <artur.glier@schibsted.pl>
2015-06-16 09:26:38 +0200

    Added proof-of-concept for app installed detection

---
+++ app/src/main/java/com/arturglier/mobile/android/experiments/appdetection/ArticleActivity.java
@@ -0,0 +1,14 @@
+package com.arturglier.mobile.android.experiments.appdetection;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArticleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_article);
+    }
+}
